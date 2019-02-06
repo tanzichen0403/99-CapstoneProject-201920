0187a94841dec4a3b9c1746e1a58639bce75b12f
liuh8 <liuh8@rose-hulman.edu>
2019-02-06 14:52:08 -0500

    calibrate_arm test code

--- src/m3_run_this_on_robot.py
+++ src/m3_run_this_on_robot.py
@@ -16,6 +16,17 @@ def main():
       1. Makes the EV3 robot to various things.
       2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
     """
+    # run_test_arm()
+    run_test_calibrate_arm()
+
+
+def run_test_arm():
+    robot = rosebot.RoseBot()
+    robot.arm_and_claw.raise_arm()
+
+def run_test_calibrate_arm():
+    robot = rosebot.RoseBot()
+    robot.arm_and_claw.calibrate_arm()
 
 
 # -----------------------------------------------------------------------------
