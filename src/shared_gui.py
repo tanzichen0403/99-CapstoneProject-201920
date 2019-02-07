55c65bbc20e93306b72877b643d8c7f3e4be36a5
Tanz <tanz@rose-hulman.edu>
2019-02-07 14:16:36 -0500

    Merge branch 'master' of C:\Users\Tanz\PycharmProjects\99-CapstoneProject-201920 with conflicts.

--- src/shared_gui.py
+++ src/shared_gui.py
@@ -164,7 +164,8 @@ def handle_forward(left_entry_box, right_entry_box, mqtt_sender):
       :type  right_entry_box:  ttk.Entry
       :type  mqtt_sender:      com.MqttClient
     """
-
+    print('forward',left_entry_box.get(),right_entry_box.get())
+    mqtt_sender.send_message('forward',[left_entry_box.get(),right_entry_box.get()])
 
 def handle_backward(left_entry_box, right_entry_box, mqtt_sender):
     """
